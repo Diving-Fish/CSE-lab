831c013c2dfdc8c1a39186ec4decf4adb3681646
Diving-Fish <dfyshisb@163.com>
2019-11-12 02:00:49 +0800

    solution for lab3

--- lock_server_cache.h
+++ lock_server_cache.h
@@ -3,20 +3,45 @@
 
 #include <string>
 
-
 #include <map>
+#include <set>
 #include "lock_protocol.h"
 #include "rpc.h"
 #include "lock_server.h"
 
+class lock_server_cache
+{
 
-class lock_server_cache {
- private:
+private:
   int nacquire;
- public:
+  enum status
+  {
+    NONE,
+    LOCKED,
+    REVOKING,
+    RETRYING
+  };
+
+  typedef struct lock_state
+  {
+    std::string owner;
+    std::string retrying;
+    std::set<std::string> waiting;
+    status state;
+  } lock_state_t;
+
+  std::map<lock_protocol::lockid_t, lock_state_t *> lock_map;
+  pthread_mutex_t mutex;
+
+public:
   lock_server_cache();
+
+  ~lock_server_cache();
+
   lock_protocol::status stat(lock_protocol::lockid_t, int &);
+
   int acquire(lock_protocol::lockid_t, std::string id, int &);
+
   int release(lock_protocol::lockid_t, std::string id, int &);
 };
 
